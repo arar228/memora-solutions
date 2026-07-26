62c59e71628e03a6b2d495d3481ac453f9c8a1ab
arar228 <arar228@users.noreply.github.com>
2026-07-27 02:32:36 +0300

    Smooth Pomodoro ninja animation with in-between frames

--- memora-pomodoro/src/renderer/assets.ts
+++ memora-pomodoro/src/renderer/assets.ts
@@ -1,2 +1,2 @@
 export const APP_ICON_URL = new URL('../../assets/icon.png', import.meta.url).href;
-export const NINJA_TOMATO_SPRITES_URL = new URL('../../assets/ninja-tomato-sprites.png', import.meta.url).href;
+export const NINJA_TOMATO_SPRITES_URL = new URL('../../assets/ninja-tomato-sprites-8f.png', import.meta.url).href;
